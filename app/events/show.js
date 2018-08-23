42e411ff9cde5e8dab6ba4d9d236e4d6effc17e6
Daniel Coelho <coelhodesigns@gmail.com>
2018-08-23 18:59:31 +0100

    upvotes

--- app/events/show.js
+++ app/events/show.js
@@ -1,4 +1,4 @@
-import { fetchQuestion } from '../fetch_data/questions.js';
+import { fetchQuestion, upVoteAnswer, downVoteAnswer } from '../fetch_data/questions.js';
 import { addIndexOnClick } from './index.js';
 // import { createAnswer } from '../app/events/create.js';
 
@@ -16,8 +16,6 @@ import CodeBlock from 'quill/formats/code';
 import Snow from 'quill/themes/snow';
 import Underline from 'quill/formats/underline';
 
-
-
 Quill.register({
   'modules/toolbar': Toolbar,
   'formats/bold': Bold,
@@ -27,8 +25,6 @@ Quill.register({
   'themes/snow': Snow,
   'formats/underline': Underline,
 });
-
-
 
 export default Quill;
 
@@ -43,7 +39,6 @@ function insertQuillContent(quillElements) {
     editor.setContents( JSON.parse(element[1]));
     editor.enable(false);
   })
-
 };
 
 
@@ -83,11 +78,12 @@ export function show(id) {
     var timeStamp = response.data.question.time;
     var ownerName = response.data.question.name;
     var questionId = response.data.question.id;
+    var avatar = response.data.question.avatar;
 
     //INJECT QUESTION INFO
 
     var showQuestion = 'question-content-'+ questionId;
-    showContent = '<div class="card mb-0 mt-2"> <div class="card-body auxilium-wrapper"> <!-- question --> <div class="auxilium-question d-flex flex-row bd-highlight justify-content-between align-content-center"> <div class="mr-5"> <img src="https://avatars2.githubusercontent.com/u/9859208?v=4" class="rounded-circle" style="max-width:50px;"> </div> <div class="pr-4" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Posted '+ timeStamp +' by '+ ownerName +'</p> <h2 class="mt-0">'+ questionTitle +'</h2><div id="' + showQuestion + '"> </div> <div class="d-flex flex-row justify-content-between align-content-center p-3"> <h2 class="text-secondary mt-auto">'+ numberOfAnswers +' Answers</h2> <a class="btn btn-default mt-auto" style="max-height:40px;" href"" id="createAnswer">Post an answer</a> </div> </div> </div> <!-- question end --> </div></div>';
+    showContent = '<div class="card mb-0 mt-2"> <div class="card-body auxilium-wrapper"> <!-- question --> <div class="auxilium-question d-flex flex-row bd-highlight justify-content-between align-content-center"> <div class="mr-5"> <img src="'+ avatar +'" class="rounded-circle" style="max-width:50px;"> </div> <div class="pr-4" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Posted '+ timeStamp +' by '+ ownerName +'</p> <h2 class="mt-0">'+ questionTitle +'</h2><div id="' + showQuestion + '"> </div> <div class="d-flex flex-row justify-content-between align-content-center p-3"> <h2 class="text-secondary mt-auto">'+ numberOfAnswers +' Answers</h2> <a class="btn btn-default mt-auto" style="max-height:40px;" href"" id="createAnswer">Post an answer</a> </div> </div> </div> <!-- question end --> </div></div>';
 
 
     var quillAnswerElements = [];
@@ -103,28 +99,33 @@ export function show(id) {
       var ownerName = response.data.question.answers[i].name;
       var votesValue = response.data.question.answers[i].votes;
       var answerId = response.data.question.answers[i].id;
+      var avatar = response.data.question.answers[i].avatar;
+      var upVoteId = "up-id-" + answerId;
+      var downVoteId = "down-id-" + answerId;
 
       //INJECT ANSWER INFO
       var showAnswer = 'answer-content-'+ answerId;
       quillAnswerElements.push([showAnswer, answerContent])
-      showContent += '<div class="auxilium-answer d-flex flex-row bd-highlight justify-content-between align-content-center mt-4"> <div class="mr-5"> <img src="https://avatars2.githubusercontent.com/u/9859208?v=4" class="rounded-circle" style="max-width:35px;"> <div class="mt-5" style="text-align: center;"> <a href=""><i class="fas fa-caret-up text-secondary" style="font-size: 3em;"></i></a> <h2 class="text-secondary m-0">'+ votesValue +'</h2> <a href=""><i class="fas fa-caret-down text-secondary" style="font-size: 3em;"></i></a> </div> </div> <div class="pr-4 d-flex flex-column justify-content-between" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Answered '+ timeStamp +' by '+ ownerName +'</p> <div id="' + showAnswer + '"> </div> <div class="d-flex flex-row justify-content-between align-content-center p-3"> <div class="mt-auto"> <p class="mb-0">'+ numberOfComments +' Comments</p> </div> <div class="mt-auto mb-auto"> <a class="btn btn-default mt-auto" style="max-height:40px;" href="">Post a comment</a> </div> </div> </div> </div>';
+      showContent += '<div class="auxilium-answer d-flex flex-row bd-highlight justify-content-between align-content-center mt-4"> <div class="mr-5"> <img src="'+ avatar +'" class="rounded-circle" style="max-width:35px;"> <div class="mt-5" style="text-align: center;"> <a href="" answer-id="' + answerId + '" class="up-vote-btn '+ upVoteId +'"><i class="fas fa-caret-up text-secondary" style="font-size: 3em;"></i></a> <h2 class="text-secondary m-0">'+ votesValue +'</h2> <a href="" answer-id="' + answerId + '" class="'+ downVoteId +'"><i class="fas fa-caret-down text-secondary" style="font-size: 3em;"></i></a> </div> </div> <div class="pr-4 d-flex flex-column justify-content-between" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Answered '+ timeStamp +' by '+ ownerName +'</p> <div id="' + showAnswer + '"> </div> <div class="d-flex flex-row justify-content-between align-content-center p-3"> <div class="mt-auto"> <p class="mb-0">'+ numberOfComments +' Comments</p> </div> <div class="mt-auto mb-auto"> <a class="btn btn-default mt-auto" answer-id="' + answerId + '" style="max-height:40px;" href="">Post a comment</a> </div> </div> </div> </div>';
 
       for (var j = 0; j < numberOfComments; j++){
         var timeStamp = response.data.question.answers[i].comments[j].time;
         var commentContent = response.data.question.answers[i].comments[j].content;
         var ownerName = response.data.question.answers[i].comments[j].name;
         var commentId = response.data.question.answers[i].comments[j].id;
+        var avatar = response.data.question.answers[i].comments[j].avatar;
 
         //INJECT COMMENT INFO
         var showComment = 'comment-content-'+ commentId;
-        quillCommentElements.push([showComment, answerContent])
-        showContent += '<div class="ml-5"> <div class="pt-4 d-flex flex-row bd-highlight justify-content-between align-content-center ml-5" style="border-top: 1px solid #dee2e6;"> <div class="mr-5"> <img src="https://avatars2.githubusercontent.com/u/9859208?v=4" class="rounded-circle" style="max-width: 30px;"> </div> <div class="pr-4" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Answered '+ timeStamp +' by '+ ownerName +'</p> <div id="' + showComment + '"> </div> </div> </div> </div>';
+        quillCommentElements.push([showComment, commentContent])
+        showContent += '<div class="ml-5"> <div class="pt-4 d-flex flex-row bd-highlight justify-content-between align-content-center ml-5" style="border-top: 1px solid #dee2e6;"> <div class="mr-5"> <img src="'+ avatar +'" class="rounded-circle" style="max-width: 30px;"> </div> <div class="pr-4" style="flex-grow: 1; width: 80%"> <p class="text-secondary font-italic font-weight-light mb-3 mt-0" style="font-size:0.8em;">Answered '+ timeStamp +' by '+ ownerName +'</p> <div id="' + showComment + '"> </div> </div> </div> </div>';
       }
 
       showContent += '</div></div>';
 
     };
     syllabus_content.innerHTML = showContent;
+
 
 
     insertContent(questionContent, showQuestion);
@@ -135,12 +136,66 @@ export function show(id) {
 
     // console.log(questionTitle + timeStamp + ownerName + questionId +numberOfAnswers)
     newAnswerOnClick(questionId);
-  };
 
+    var me = document.querySelector(".me");
+    var me_parsed = me.innerHTML.substr(me.innerHTML.indexOf("by ") + 3);
+    var userName = me_parsed.substr(0, me_parsed.indexOf(" "));
+
+    for (var i = 0; i < numberOfAnswers; i++) {
+      var answerId = response.data.question.answers[i].id;
+      var upVoteId = ".up-id-" + answerId;
+      var downVoteId = ".down-id-" + answerId;
+
+      if(response.data.question.answers[i].answer_votes){
+
+        var btnUpVote = document.querySelector(upVoteId);
+
+        btnUpVote.addEventListener('click', function() {
+            event.preventDefault();
+        });
+
+        var btnDownVote = document.querySelector(downVoteId);
+
+        btnDownVote.addEventListener('click', function() {
+            event.preventDefault();
+        });
+
+        var hasVoted = response.data.question.answers[i].answer_votes.vote;
+
+        if(hasVoted == 1){
+          console.log("activate upvote");
+        } else if (hasVoted == -1){
+          console.log("activate downvote");
+        };
+      } else{
+                    console.log("teste else" );
+        var btnUpVote = document.querySelector(upVoteId);
+
+        btnUpVote.addEventListener('click', function() {
+            event.preventDefault();
+            upVoteAnswer(questionId, event.target.parentNode.getAttribute('answer-id'), userName);
+            console.log(questionId);
+            show(questionId);
+        });
+
+        var btnDownVote = document.querySelector(downVoteId);
+
+        btnDownVote.addEventListener('click', function() {
+            event.preventDefault();
+            downVoteAnswer(questionId, event.target.parentNode.getAttribute('answer-id'), userName);
+            console.log(questionId);
+            show(questionId);
+        });
+      };
+    }
+
+
+  };
 
   var me = document.querySelector(".me");
   var me_parsed = me.innerHTML.substr(me.innerHTML.indexOf("by ") + 3);
   var userName = me_parsed.substr(0, me_parsed.indexOf(" "));
+
   fetchQuestion(id, userName).then(insertQuestion);
 };
 
@@ -150,6 +205,6 @@ function addBackBtn() {
   var button = '<a class="auxilium btn btn-default btn-question" style="margin-top: 5px;" id="forum-link" href="#">Back to questions</a>';
   div.removeChild(div.lastChild);
   div.insertAdjacentHTML('beforeend', button);
-
   addIndexOnClick();
 }
+
